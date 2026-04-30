4b914239c04a165988a7718b8cf590099cf14a5a
psodage <psodage@gmail.com>
2026-04-30 23:31:59 +0530

    fresh upload

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["dandelion-tall-numerator.ngrok-free.dev"],
+    allowedHosts: ["antisocial-1pig.onrender.com"],
   },
 });
